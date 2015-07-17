257f0b3a72e62a8dee4e5e8b25ebea0cf7bf6191
nazarovits <istvan.nazarovits@gmail.com>
2015-07-17 09:47:20 +0200

    rds host

--- config/development.js
+++ config/development.js
@@ -1,7 +1,7 @@
 ﻿process.env.HOST = 'http://134.249.164.53:8850/';
 process.env.PORT = '8850';
 
-process.env.RDS_HOSTNAME = '134.249.164.53';
+process.env.RDS_HOSTNAME = '192.168.88.250';
 process.env.RDS_USERNAME = 'postgres';
 process.env.RDS_PASSWORD = 'postgres';
 process.env.RDS_PORT = 5432;
