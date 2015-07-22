76da16ebe5be49d7d9a7b1276e5b2dace7287210
AndreyFizer <GIThub31235677>
2015-07-22 15:06:07 +0300

    comm_router

---
+++ public/js/constants/statuses.js
@@ -0,0 +1,8 @@
+// Filename: app.js
+define(function () {
+    return {
+        DELETED    : 0,    // 'deleted',
+        ACTIVE     : 1,    // 'active',
+        SUBSCRIBED : 2     // 'subscribed'
+    }
+});
