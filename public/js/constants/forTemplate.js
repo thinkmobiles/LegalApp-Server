985a1e1ceeff32f33d883e7c1c2521118d12e757
AndreyFizer <GIThub31235677>
2015-08-20 09:22:36 +0300

    comm_edit_usr

---
+++ public/js/constants/forTemplate.js
@@ -0,0 +1,12 @@
+/**
+ * Created by root on 19.08.15.
+ */
+
+define(function () {
+    return {
+        FIRST_NAME: 'profile.first_name',
+        LAST_NAME: 'profile.last_name',
+        EMAIL: 'email',
+        PHONE: 'profile.phone'
+    }
+});
