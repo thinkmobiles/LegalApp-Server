3235d892c537e79969cd87b6c6535243411f77e8
Killer57575 <Killer57575@gmail.com>
2015-09-03 11:32:23 +0300

    fix

--- helpers/socketEvents.js
+++ helpers/socketEvents.js
@@ -6,7 +6,7 @@ var socketEvents = function (io) {
     io.on('connection', function( socket ) {
         console.log('>>>user connected to socket');
         /*socket.on('signUp', function(data){
-            console.log('===Cпрацювала подія сокета signUp===отримав ='+data);
+            console.log('===event signUp===get from UI ='+data);
         })*/
 
     });
